651b68d60487191dbb2c3bb7b6d7ceb5ec192311
evanharris <evan.tyler.harris@gmail.com>
2015-02-09 21:32:35 -0800

    inject build migrated

--- gulp/tasks/inject.js
+++ gulp/tasks/inject.js
@@ -1,29 +1,32 @@
-module.exports = {
-  injectBuild: function() {
-    gulp         = global.gulp;
-    gulpInject   = global.gulpInject;
-    foldersBuild = global.options.folders.build;
-    filesHtml    = global.options.files.html;
-    filesJs      = global.options.files.js;
-    filesCss     = global.options.files.css;
-    angularFilesort = global.gulpAngularFileSort;
-    vendorJs     = global.options.vendor.js.files;
+'use strict';
 
-    files = [];
+var gulp        = require('gulp');
+var gulpInject  = require('gulp-inject');
+var config      = global.config;
+var foldersBuild = config.folders.build;
+var filesHtml    = config.files.html;
+var filesJs      = config.files.js;
+var filesCss     = config.files.css;
+var angularFilesort = require('gulp-angular-filesort');
+var vendorJs     = config.vendor.js.files;
+
+gulp.task('inject:build',function() {
+
+    var files = [];
     for (var i = 0; i < vendorJs.length; i++) {
       var file = vendorJs[i];
       var fileSplit = file.split('/');
       files.push( foldersBuild + '/vendor/' + fileSplit[fileSplit.length - 1]);
     }
-    console.log(files);
 
-    gulp.src( foldersBuild + '/' + filesHtml )
+    return gulp.src( foldersBuild + '/' + filesHtml )
       .pipe( gulpInject( gulp.src( files ), {name:'vendor', relative: true}))
       .pipe( gulpInject( gulp.src( foldersBuild + '/app/' + filesJs).pipe(angularFilesort()), {name:'app', relative: true}))
       .pipe( gulpInject( gulp.src( foldersBuild + '/' + filesCss), {relative: true}))
       .pipe( gulp.dest( foldersBuild ));
-  },
-  injectDist: function() {
+});
+
+gulp.task('inject:dist', function() {
     // gulp         = global.gulp;
     // gulpInject   = global.gulpInject;
     // foldersDist  = global.options.folders.dist;
@@ -35,5 +38,4 @@ module.exports = {
     //   .pipe( gulpInject( gulp.src( foldersDist + '/' + filesJs,  {read: false}), {relative: true}))
     //   .pipe( gulpInject( gulp.src( foldersDist + '/' + filesCss, {read: false}), {relative: true}))
     //   .pipe( gulp.dest( foldersDist ));
-  }
-}
+});
