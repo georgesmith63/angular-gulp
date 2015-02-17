1de5af7bdad8d751f98152dddabaa51e13aa4358
Craig Nishina <craig.nishina@gmail.com>
2015-02-17 02:34:01 -0800

    'gulp watch' method working with browser sync

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,10 @@
-// var gulp         = require('gulp');
-// var browserSync  = require('browser-sync');
-// var runSequence  = require('run-sequence');
-// var config       = global.config;
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync');
+var runSequence  = require('run-sequence');
+
+var config       = global.config;
+var src          = config.app.src;
+var build        = config.app.build;
 // var foldersSrc   = config.folders.src;
 // var foldersBuild = config.folders.build;
 // var files        = "**/*";
@@ -10,18 +13,36 @@
 // var filesHtml    = config.files.html;
 // var filesTpl     = config.files.tpl;
 //
-// gulp.task('watch:build', function() {
-//     runSequence('clean:build', 'copy:build', 'less:build', 'html2js:build', 'inject:build', 'browserSync:build');
-//   return gulp.watch(foldersSrc + '/' + files, function() {
-//     runSequence('clean:build', 'copy:build', 'less:build', 'html2js:build', 'inject:build', 'browserSync:build');
-//   });
-// });
-//
-// gulp.task('browserSync:build', function() {
-//   browserSync({
-//     server: {
-//       baseDir: foldersBuild
-//     }
-//   });
-//   return browserSync.reload();
-// });
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: build.folder
+        }
+    });
+});
+
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task('build-and-reload', function () {
+    return runSequence(
+      'copy:build',
+      'bootstrap:build:copy:js',
+      'bootstrap:build:copy:css',
+      'bootstrap:build:copy:fonts',
+      'bootstrap:build:inject',
+      'angular:build:copy:js',
+      'angular:build:inject',
+      'less:build',
+      'html2js:build',
+      'inject:build',
+      'reload');
+});
+
+// Default task to be run with `gulp`
+gulp.task('watch', ['build', 'browser-sync'], function () {
+    gulp.watch(src.folder + '/**/*', ['build-and-reload']);
+});
+
+gulp.task('reload', function() {
+  return browserSync.reload();
+});
